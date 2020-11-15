cd70a191ab308e9cd700f4f7c068b97d3ba2ce0a
NikVogri <55229210+NikVogri@users.noreply.github.com>
2020-11-15 15:17:25 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import { usePubNub } from "pubnub-react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [onlineUsers, setOnlineUsers] = useState([]);
+
+  const pubnub = usePubNub();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    pubnub.setUUID(name);
+    pubnub.subscribe({
+      channels: ["global"],
+    });
+  };
+
+  useEffect(() => {
+    const refreshInterval = setInterval(() => {
+      pubnub.hereNow(
+        {
+          channels: ["global"],
+          includeUUIDs: true,
+        },
+        (_, response) => {
+          console.log(_, response);
+          if (response !== null) {
+            const onlineUserIds = response.channels.global.occupants.map(
+              (onlineUser) => onlineUser.uuid
+            );
+            setOnlineUsers(onlineUserIds);
+          }
+        }
+      );
+    }, 2000);
+
+    return () => {
+      clearInterval(refreshInterval);
+      leaveApplication();
+    };
+  }, [pubnub]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={submitHandler}>
+        <input onChange={(e) => setName(e.target.value)} value={name} />
+        <button type="submit">Add Me</button>
+      </form>
+      <hr />
+      <p>
+        Currently Online:{" "}
+        {onlineUsers.map((user) => (
+          <span>{user} </span>
+        ))}
+      </p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import PubNub from "pubnub";
+import { PubNubProvider } from "pubnub-react";
+
+const pubnub = new PubNub({
+  publishKey: "pub-c-aefae728-45a7-4251-9bc5-2748793d0464",
+  subscribeKey: "sub-c-2f65dd9c-2741-11eb-862a-82af91a3b28d",
+});
+
+const leaveApplication = () => {
+  pubnub.unsubscribeAll();
+};
+
+window.addEventListener("beforeunload", leaveApplication);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <PubNubProvider client={pubnub}>
+      <App />
+    </PubNubProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
